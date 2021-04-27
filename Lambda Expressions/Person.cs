bfc2543751c7ed8175fa44e80c891679fb94d554
ok97 <omkhawshi0@gmail.com>
2021-04-27 22:06:30 +0530

    Added: Create Person class with SSN...created list

---
+++ Lambda Expressions/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expressions
+{
+    class Person
+    {      
+        
+        public int SNN { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }       
+
+    }
+}
